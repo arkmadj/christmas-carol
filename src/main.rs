d52e6191b9d2c1d0d0562080c351ca6250db62ff
arkmadj <ahmadjinadu@gmail.com>
2024-11-27 08:15:25 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let gifts = [
+        "A partridge in a pear tree",
+        "Two turtle doves",
+        "Three French hens",
+        "Four calling birds",
+        "Five golden rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+
+    let ordinals = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+
+    for i in 0..12 {
+        println!("On the {} day of Christmas,", ordinals[i]);
+        println!("my true love gave to me");
+        for j in 0..i + 1 {
+            println!("{}", gifts[j]);
+        }
+        println!();
+    }
+}
